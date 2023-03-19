9f3d986eceb9fdd92b9525dac1c8864f465418d0
KaiOsU8 <kokokovachev10@gmail.com>
2023-03-19 15:50:35 +0100

    finished styling quiz page

--- PHP-stuff/quiz.php
+++ PHP-stuff/quiz.php
@@ -32,12 +32,12 @@
             overflow: hidden;
         }
     </style>
-    
+    <h1 id="quiz-titel">Quiz</h1>
     <div id="quiz">
         <form action="antwoord.php " method="post">
             <div id="vraag1">
-                <h6>Hoor je (minstens 1 keer per week) dingen die anderen niet horen, <br>
-                zoals ongewone geluiden, stemmen van fluisterende of pratende mensen?</h6>
+                <h3>Hoor je (minstens 1 keer per week) dingen die anderen niet horen, <br>
+                zoals ongewone geluiden, stemmen van fluisterende of pratende mensen?</h3>
                 <div>
                     <input type="radio" name="vraag-1-punt" id="vraag-1-punt" value="4" />
                     <label for="vraag-1-punt">nooit</label>
@@ -55,7 +55,7 @@
                     <label for="vraag-1-punt">altijd</label>
                 </div>
             </div>
-            <button id="v1-button">volgende vraag</button>
+            <div id="v1-button" class="button-style" onclick='button1()'>volgende vraag</div>   
             <div id="vraag2">
                 <h3>Hoor je (minstens 1 keer per week) stemmen tegen elkaar praten, terwijl je alleen bent?</h3>
                 <div>
@@ -75,7 +75,7 @@
                     <label for="vraag-2-punt">altijd</label>
                 </div>
             </div>
-            <div id="v2-button">volgende vraag</div>
+            <div id="v2-button" class="button-style" onclick='button2()'>volgende vraag</div>
             
             <div id="vraag3">
                 <h3>Zie je dingen die anderen niet kunnen zien, zoals flitsen, schaduwen, voorwerpen, vormen van mensen of dieren?</h3>
@@ -96,7 +96,7 @@
                     <label for="vraag-3-punt">altijd</label>
                 </div>
             </div>
-            <div id="v3-button">volgende vraag</div>
+            <div id="v3-button" class="button-style" onclick='button3()'>volgende vraag</div>
             
             <div id="vraag4">
                 <h3>Heb je het idee dat je achtervolgd wordt?</h3>
@@ -117,7 +117,7 @@
                     <label for="vraag-4-punt">altijd</label>
                 </div>
             </div>
-            <div id="v4-button">volgende vraag</div>
+            <div id="v4-button" class="button-style" onclick='button4()'>volgende vraag</div>
             
             <div id="vraag5">
                 <h3>Heb je het idee dat anderen je kwaad willen doen? Dat ze je misschien willen vergiftigen of zelfs willen vermoorden?</h3>
@@ -138,7 +138,7 @@
                     <label for="vraag-5-punt">altijd</label>
                 </div>
             </div>
-            <div id="v5-button">volgende vraag</div>
+            <div id="v5-button" class="button-style" onclick='button5()'>volgende vraag</div>
             <div id="vraag6">
                 <h3>Denk je dat er een complot tegen je wordt gesmeed?</h3>
                 <div>
@@ -158,7 +158,7 @@
                     <label for="vraag-6-punt">altijd</label>
                 </div>
             </div>
-            <div id="v6-button">volgende vraag</div>
+            <div id="v6-button" class="button-style" onclick='button6()'>volgende vraag</div>
             <div id="vraag7">
                 <h3>Heb je het gevoel dat een dubbelganger de plaats heeft ingenomen van een familielid, vriend of bekende?</h3>
                 <div>
@@ -178,7 +178,7 @@
                     <label for="vraag-7-punt">altijd</label>
                 </div>
             </div>
-            <div id="v7-button">volgende vraag</div>
+            <div id="v7-button" class="button-style" onclick='button7()'>volgende vraag</div>
             <div id="vraag8">
                 <h3>Denk je (minstens 1 keer per week) dat mensen je gedachten kunnen lezen?</h3>
                 <div>
@@ -198,7 +198,7 @@
                     <label for="vraag-8-punt">altijd</label>
                 </div>
             </div>
-            <div id="v8-button">volgende vraag</div>
+            <div id="v8-button" class="button-style" onclick='button8()'>volgende vraag</div>
             <div id="vraag9">
                 <h3>Worden je gedachten hardop uitgezonden, zodat anderen weten wat je denkt?</h3>
                 <div>
@@ -218,7 +218,7 @@
                     <label for="vraag-9-punt">altijd</label>
                 </div>
             </div>
-            <div id="v9-button">volgende vraag</div>
+            <div id="v9-button" class="button-style" onclick='button9()'>volgende vraag</div>
             <div id="vraag10">
                 <h3>Heb je het idee dat iemand telepathisch met je communiceert?</h3>
                 <div>
@@ -238,7 +238,7 @@
                     <label for="vraag-10-punt">altijd</label>
                 </div>
             </div>
-            <div id="v10-button">volgende vraag</div>
+            <div id="v10-button" class="button-style" onclick='button10()'>volgende vraag</div>
             <div id="vraag11">
                 <h3>Heb je het idee dat er gedachten uit je hoofd worden gehaald?</h3>
                 <div>
@@ -258,10 +258,10 @@
                     <label for="vraag-11-punt">altijd</label>
                 </div>
             </div>
-            <div id="submit-button"><input  type="submit" value="Verstuur" /></div>
+            <input id="submit-button" class="button-style" type="submit" value="Verstuur" />
             
         </form>
     </div>
 </body>
-
+<script src="../javascript/quizScript.js"></script>
 </html>
